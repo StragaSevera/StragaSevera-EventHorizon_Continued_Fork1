fdf21981e732fe8d02bcb328d5e6fe8788d8e98e
calenbraga <calenbraga@gmail.com>
2024-08-18 11:22:20 -0500

    Update config.lua for TWW

--- EventHorizon_Druid/config.lua
+++ EventHorizon_Druid/config.lua
@@ -2,55 +2,43 @@ local usemouseover = true    -- Make this false or nil (or just delete the line 
 
 function EventHorizon:InitializeClass()
   self.config.gcdSpellID = 768 -- Cat Form
+-- self.config.gcdSpellID = 61304 -- Special GCD spell cd id??
   self.config.hastedSpellID = {50769,10} -- Revive
   self.config.past = -1.5 -- Number of seconds to show in the past. Default = -1.5
   self.config.future = 12 -- Number of seconds to show in the future. Default = 12
 
--- NOTE: bars appear here in the order of the talent trees
--- which is usually NOT what you want.  You should rearrange
--- them in a more logical order
 
 -- specializations
-  local balance = 1
-  local feral = 2
-  local guardian = 3
-  local restoration = 4
+  balance = 1
+  feral = 2
+  guardian = 3
+  restoration = 4
 
 -- stances
-  local human = 0
-  local bear = 1
-  local cat = 2
-  local travel = 3
-  local moonkin = 4
-  local flying = 5
+  human = 0
+  bear = 1
+  cat = 2
+  travel = 3
+  moonkin = 4
+  flying = 5
 
--- baseline skills
 
-  -- Barkskin
+-- Feral bars
+
+  -- Feral Frenzy with Smoldering Frenzy
   self:newSpell({
-    cooldown = 22812,
-    playerbuff = 22812,
-     barcolors = {
-     playerbuff  = {141/255, 070/255, 030/255, 0.6},
-     cooldown    = {171/255, 191/255, 181/255, 0.6}
-     },
-  })
-
-  -- Moonfire with Force of Nature or Warrior of Elune cooldown at half height.
-  self:newSpell({
-    stance = {human,bear,moonkin,5},
-    debuff = {164812,2},
+    requiredTalent = 274837,
+    stance = cat,
+	playerbuff = 422751, 					
+    debuff = {274837,3},
     refreshable = true,
-    cooldown = {202425, 205636},
-    smallCooldown = true,
-     barcolors = {
-     debuffmine  = {050/255, 101/255, 252/255, 0.5},
-     cooldown    = {171/255, 191/255, 181/255, 0.6},
-     },
+    cooldown = 274837,
+    barcolors = {
+     playerbuff  = {222/255, 161/255, 050/255, 0.5},
+     debuffmine  = {222/255, 128/255, 010/255, 0.5},
+     cooldown    = {222/255, 171/255, 171/255, 0.4}
+    },
   })
-
-
--- general druid tree skills (with added stuff)
 
   -- Rake with Feral Frenzy CD at half height if talent chosen.
   self:newSpell({
@@ -66,63 +54,10 @@ function EventHorizon:InitializeClass()
      },
   })
 
-  -- Frenzied Regen & Earthwarden if talented.
+-- Primal Wrath
   self:newSpell({
-    requiredTalent = 22842,
-    stance = bear,
-    requiresLevel = 50,
-    playerbuff = {{22842}, {203975}},
-    recharge = 22842,
-     barcolors = {
-     playerbuff  = {101/255, 181/255, 141/255, 0.8},
-     recharge    = {030/255, 222/255, 040/255, 0.4}
-     },
-  })
-
-  -- Rejuvenation + Swiftmend
-  self:newSpell({
-    requiredTalent = 774,
-    stance = {0,4},
-    playerbuff = {774,3},
-    cooldown = 18562,
-    smallCooldown = true,
-    auraunit = usemouseover and 'mouseover' or 'target',
-    refreshable = true,
-     barcolors = {
-     cooldown    = {171/255, 191/255, 181/255, 0.6},
-     playerbuff  = {232/255, 066/255, 202/255, 0.6}
-     }
-  })
-
-  -- Thrash (cat) plus Brutal Slash recharge
-  self:newSpell({
-    requiredTalent = 106832,
-    stance = cat,
-    debuff = {106830,3},
-    refreshable = true,
-    recharge = 202028,
-    barcolors = {
-     debuffmine  = {222/255, 128/255, 010/255, 0.5},
-     recharge    = {222/255, 171/255, 171/255, 0.4}
-    },
-  })
-
-  -- Thrash (bear, has CD)
-  self:newSpell({
-    requiredTalent = 106832,
-    stance = bear,
-    debuff = {77758,3},
-    refreshable = true,
-    cooldown = 77758,
-    barcolors = {
-     debuffmine  = {222/255, 128/255, 010/255, 0.5},
-     cooldown    = {222/255, 171/255, 171/255, 0.4}
-    },
-  })
-
-  -- Rip
-  self:newSpell({
-    requiredTalent = 1079,
+    requiredTalent = 285381,
+	icon = 285381,
     stance = cat,
     debuff = {1079,2},
     refreshable = true,
@@ -131,19 +66,128 @@ function EventHorizon:InitializeClass()
     },
   })
 
-  -- Ironfur with Mighty Bash or Incapacitating roar cooldown at half height.
+-- Rip
   self:newSpell({
-    requiredTalent = 192081,
-    stance = bear,
-    playerbuff = 192081,
-    cooldown = {5211, 99},
-    smallCooldown = true,
+    requiredTalent = 1079,
+	requiredTalentUnselected = 285381,
+    stance = cat,
+    debuff = {1079,2},
+    refreshable = true,
     barcolors = {
-     playerbuff  = {111/255, 111/255, 151/255, 0.6},
-     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     debuffmine  = {252/255, 000/255, 060/255, 0.5}
     },
   })
-   
+
+-- Thrash (cat) plus Brutal Slash recharge
+  self:newSpell({
+    stance = cat,
+    debuff = {106832,3},
+    refreshable = true,
+    recharge = 202028,
+    barcolors = {
+     debuffmine  = {222/255, 128/255, 010/255, 0.5},
+     recharge    = {222/255, 171/255, 171/255, 0.4}
+    },
+  })
+
+  -- Moonfire in cat form, only with talent
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 155580,
+    stance = cat,
+    debuff = {155625,2},
+    refreshable = true,
+    barcolors = {
+     debuffmine  = {050/255, 100/255, 255/255, 0.5}
+    },
+  })
+
+  -- Tiger's Fury
+  self:newSpell({
+    requiredTree = feral,
+    stance = cat,
+    cooldown = 5217,
+    playerbuff = {5217, 6},
+     barcolors = {
+     playerbuff  = {252/255, 232/255, 090/255, 0.6},
+     cooldown    = {252/255, 232/255, 090/255, 0.4}
+    },
+  })
+
+  -- Adaptive Swarm
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 391888,
+    stance = cat,
+    cooldown = 391888,
+    debuff = {391889, 3},
+    barcolors = {
+     debuffmine  = {000/255, 096/255, 144/255, 0.6},
+     cooldown = {224/255, 224/255, 224/255, 0.4},
+    },
+  })
+  
+  -- Bloodtalons + convoke CD
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 319439,
+    stance = cat,
+    cooldown = 391528,
+    playerbuff = 319439,
+    barcolors = {
+     playerbuff  = {030/255, 131/255, 000/255, 0.5}
+    },
+  })
+
+  -- Berserk
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 106951,
+    requiredTalentUnselected = 102543,
+    stance = cat,
+    cooldown = 106951,
+    playerbuff = 106951,
+  })
+
+  -- Incarnation
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 102543,
+    stance = cat,
+    cooldown = 102543,
+    playerbuff = 102543,
+  })
+
+  -- Omen/Moment of Clarity & Survival Instinct recharge
+  self:newSpell({
+    requiredTree = feral,
+    requiredTalent = 16864,
+    stance = cat,
+    playerbuff = 135700,
+    icon = 135700,
+    cooldown = 61336,
+     barcolors = {
+     playerbuff  = {040/255, 090/255, 191/255, 0.6},
+     recharge    = {232/255, 161/255, 101/255, 0.3}
+    }
+  })
+
+
+-- Balance bars
+  
+  -- Moonfire with Force of Nature or Warrior of Elune cooldown at half height.
+  self:newSpell({
+    stance = {human,bear,moonkin,5},
+    debuff = {164812,2},
+    refreshable = true,
+    cooldown = {202425, 205636},
+    smallCooldown = true,
+     barcolors = {
+     debuffmine  = {050/255, 101/255, 252/255, 0.5},
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     },
+  })
+
   -- Sunfire with Fury of Elune CD at half height if chosen.
   self:newSpell({
     requiredTalent = 93402,
@@ -157,35 +201,6 @@ function EventHorizon:InitializeClass()
      cooldown    = {171/255, 191/255, 181/255, 0.6}
     },
   })
-
-  -- Wild Growth
-  self:newSpell({
-    requiredTalent = 48438,
-    stance = human,
-    playerbuff = {48438,2},
-    cooldown = 48438,
-    smallCooldown = true,
-    auraunit = usemouseover and 'mouseover' or 'target',
-    refreshable = true,
-    barcolors = {
-     playerbuff  = {101/255, 181/255, 141/255, 0.8},
-     cooldown    = {171/255, 191/255, 181/255, 0.6}
-    }
-  })
-
-  -- Nature's Vigil
-  self:newSpell({
-    requiredTalent = 124974,
-    stance = {human,bear,cat,moonkin},
-    playerbuff = 124974,
-    cooldown = 124974,
-    barcolors = {
-     playerbuff  = {101/255, 181/255, 141/255, 0.8},
-     cooldown    = {171/255, 191/255, 181/255, 0.6}
-    },
-  })
-
--- Balance bars
 
   -- New Moon
   self:newSpell({
@@ -304,101 +319,41 @@ function EventHorizon:InitializeClass()
   })
 
   -- Convoke the Spirits CD
---[[
   self:newSpell({
     requiredTree = 1,
     requiredTalent = 391528,
-    stance = {0,1,2,4},
+    stance = {human,moonkin},
     cooldown = 391528,
-  })
-]]
-
--- Feral bars
-
-  -- Moonfire in cat form, only with talent
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 155580,
-    stance = cat,
-    debuff = {155625,2},
-    refreshable = true,
-    barcolors = {
-     debuffmine  = {050/255, 100/255, 255/255, 0.5}
-    },
-  })
-
-  -- Bloodtalons + convoke CD
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 319439,
-    stance = cat,
-    cooldown = 391528,
-    playerbuff = 319439,
-    barcolors = {
-     playerbuff  = {030/255, 131/255, 000/255, 0.5}
-    },
-  })
-
-  -- Tiger's Fury
-  self:newSpell({
-    requiredTree = feral,
-    stance = cat,
-    cooldown = 5217,
-    playerbuff = {5217, 6},
-     barcolors = {
-     playerbuff  = {252/255, 232/255, 090/255, 0.6},
-     cooldown    = {252/255, 232/255, 090/255, 0.4}
-    },
-  })
-
-  -- Berserk
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 106951,
-    requiredTalentUnselected = 102543,
-    stance = cat,
-    cooldown = 106951,
-    playerbuff = 106951,
-  })
-
-  -- Incarnation
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 102543,
-    stance = cat,
-    cooldown = 102543,
-    playerbuff = 102543,
-  })
-
-  -- Adaptive Swarm
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 391888,
-    stance = cat,
-    cooldown = 391888,
-    debuff = {391889, 3},
-    barcolors = {
-     debuffmine  = {000/255, 096/255, 144/255, 0.6},
-     cooldown = {224/255, 224/255, 224/255, 0.4},
-    },
-  })
-
-  -- Omen/Moment of Clarity & Survival Instinct recharge
-  self:newSpell({
-    requiredTree = feral,
-    requiredTalent = 16864,
-    stance = cat,
-    playerbuff = 135700,
-    icon = 135700,
-    cooldown = 61336,
-     barcolors = {
-     playerbuff  = {040/255, 090/255, 191/255, 0.6},
-     recharge    = {232/255, 161/255, 101/255, 0.3}
-    }
   })
 
 
 -- Guardian bars
+
+  -- Ironfur with Mighty Bash or Incapacitating roar cooldown at half height.
+  self:newSpell({
+    requiredTalent = 192081,
+    stance = bear,
+    playerbuff = 192081,
+    cooldown = {5211, 99},
+    smallCooldown = true,
+    barcolors = {
+     playerbuff  = {111/255, 111/255, 151/255, 0.6},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+    },
+  })
+
+  -- Thrash (bear, has CD)
+  self:newSpell({
+    requiredTalent = 106832,
+    stance = bear,
+    debuff = {77758,3},
+    refreshable = true,
+    cooldown = 77758,
+    barcolors = {
+     debuffmine  = {222/255, 128/255, 010/255, 0.5},
+     cooldown    = {222/255, 171/255, 171/255, 0.4}
+    },
+  })
 
   -- Mangle
   self:newSpell({
@@ -411,6 +366,16 @@ function EventHorizon:InitializeClass()
      debuffmine  = {190/255, 040/255, 100/255, 0.5},
      cooldown    = {171/255, 191/255, 181/255, 0.6}
     },
+  })
+
+  -- Barkskin
+  self:newSpell({
+    cooldown = 22812,
+    playerbuff = 22812,
+     barcolors = {
+     playerbuff  = {141/255, 070/255, 030/255, 0.6},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+     },
   })
 
   -- Survival Instincts
@@ -458,11 +423,68 @@ function EventHorizon:InitializeClass()
   })
 
 
+-- Healing Bars
+
+-- Frenzied Regen & Earthwarden if talented.
+  self:newSpell({
+    requiredTalent = 22842,
+    stance = bear,
+    requiresLevel = 50,
+    playerbuff = {{22842}, {203975}},
+    recharge = 22842,
+     barcolors = {
+     playerbuff  = {101/255, 181/255, 141/255, 0.8},
+     recharge    = {030/255, 222/255, 040/255, 0.4}
+     },
+  })
+
+  -- Rejuvenation + Swiftmend
+  self:newSpell({
+    requiredTalent = 774,
+    stance = {0,4},
+    playerbuff = {774,3},
+    cooldown = 18562,
+    smallCooldown = true,
+    auraunit = usemouseover and 'mouseover' or 'target',
+    refreshable = true,
+     barcolors = {
+     cooldown    = {171/255, 191/255, 181/255, 0.6},
+     playerbuff  = {232/255, 066/255, 202/255, 0.6}
+     }
+  })
+
+  -- Wild Growth
+  self:newSpell({
+    requiredTalent = 48438,
+    stance = human,
+    playerbuff = {48438,2},
+    cooldown = 48438,
+    smallCooldown = true,
+    auraunit = usemouseover and 'mouseover' or 'target',
+    refreshable = true,
+    barcolors = {
+     playerbuff  = {101/255, 181/255, 141/255, 0.8},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+    }
+  })
+
+  -- Nature's Vigil
+  self:newSpell({
+    requiredTalent = 124974,
+    stance = {human,bear,cat,moonkin},
+    playerbuff = 124974,
+    cooldown = 124974,
+    barcolors = {
+     playerbuff  = {101/255, 181/255, 141/255, 0.8},
+     cooldown    = {171/255, 191/255, 181/255, 0.6}
+    },
+  })
+
+
 -- Restoration bars
-  -- Redisign to show only the offensive spells as this looks like it will be more important in Shadowlands and I'm not aware of any healer who isn't tracking their HoT's on their party/raid frames.
+  -- Redesign to show only the offensive spells as this looks like it will be more important and I'm not aware of any healer who isn't tracking their HoT's on their party/raid frames.
   -- Longer healing CD shown at the end in increasing duration order, CD > 90 seconds not shown by default.
   -- If you'd like to return to the old design please ask in the Discord server for help if you can't do it yourself.
-
   -- Note that there are two, or more, entries for the same spell for many of these bars, this is to have them show up in the correct stances with or without Balance Affinity taken. Additionally there will be a few players out there who prefer to be a treant, all this effort was for you.
 
   -- Moonfire with spell casts (like a cast bar) and Wild Growth CD at half height.
